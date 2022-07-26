78fae64157125c7c911c087ed66be27203ca1348
songjunbao <songjunbao123>
2022-07-26 15:48:16 +0800

    # Operation Reason: 修改签名

--- src/main/java/com/example/config/email/MailSenderAutoConfiguration.java
+++ src/main/java/com/example/config/email/MailSenderAutoConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import javax.mail.Session;
 
 /**
- * @Author YSTen_SongJunBao
+ * @Author SongJunBao
  * @Description:
  * @Date 2022/5/5 11:38
  * @Version 1.0
