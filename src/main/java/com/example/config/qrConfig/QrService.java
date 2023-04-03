5a55aa9526d3c08573f6dfa1d3da47555dfee3f8
SongJunBao <songjb@pukkasoft.cn>
2023-04-03 09:23:08 +0800

    QrService

--- src/main/java/com/example/config/qrConfig/QrService.java
+++ src/main/java/com/example/config/qrConfig/QrService.java
@@ -1,5 +1,6 @@
 package com.example.config.qrConfig;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.extra.qrcode.QrCodeUtil;
 import cn.hutool.extra.qrcode.QrConfig;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
@@ -23,12 +25,23 @@ public class QrService {
     @Resource
     private QrConfig qrConfig;
 
-    public void  generateFile(String content,File file){
+    @Value("${system-parameters.qrcode_path}")
+    private String qr_path ;
+
+    public void  generateFile(String content){
         // 生成到本地文件
-        QrCodeUtil.generate(content,qrConfig,file);
+        //QrCodeUtil.generate(content,qrConfig,new File(qr_path +"/"+ DateUtil.now()));
+        QrCodeUtil.generate(content,qrConfig,new File(qr_path));
     }
     // 输出到流
     public void generateStream(String content, HttpServletResponse response) throws IOException {
+        generateFile(content);
+
         QrCodeUtil.generate(content,qrConfig,"png",response.getOutputStream());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(DateUtil.now());
+        System.out.println(DateUtil.currentSeconds());
     }
 }
